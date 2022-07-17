7cbfa3ac9dd916df58a5f75d2a3b7149218e75a3
Ziyi-T <ajb459684460@gmail.com>
2022-07-17 17:37:06 +0800

    backup

---
+++ script/sync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cp ~/.vimrc ~/MyConfig/
+cp ~/.ideavim ~/MyConfig/
+cp ~/.zshrc ~/MyConfig/
+cp -r ~/.config/alacritty ~/MyConfig/
+cp -r ~/.config/i3 ~/MyConfig/
+cp -r ~/.config/libinput-gestures.conf ~/MyConfig/
+cp -r ~/.config/polybar ~/MyConfig/
+cp -r ~/.config/rofi ~/MyConfig/
