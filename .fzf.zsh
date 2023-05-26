3b4dfe7001e934de7e284718f1c7a88f99fe459e
Ziy1-Tan <ajb459684460@gmail.com>
2023-05-26 09:39:50 +0800

    fzf path init
    Signed-off-by: Ziy1-Tan <ajb459684460@gmail.com>

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/simple/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/simple/.fzf/bin"
+if [[ ! "$PATH" == */$HOME/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/simple/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/simple/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
