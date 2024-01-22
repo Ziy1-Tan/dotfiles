c3afcc0716b62c88a42e5b7b5046a808a76e75f7
Ziy1-Tan <ajb459684460@gmail.com>
2024-01-22 17:37:32 +0800

    more keybindings

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -1,17 +1,16 @@
-alias u="sudo apt update && sudo apt upgrade"
 alias s="neofetch"
 alias cl="clear"
 alias sz="source ~/.zshrc"
-alias lg="lazygit"
 alias cmb="cmake --build build -j 12"
 alias setproxy="export http_proxy https_proxy all_proxy"
 alias unsetproxy="unset http_proxy https_proxy all_proxy"
 
 # tmux
 alias t="tmux"
-alias ta="tmux a -t 0"
+alias ta="tmux a -d -t 0"
 alias td="tmux detach"
 
+alias u="sudo apt update && sudo apt upgrade"
 if [ "$(uname -s)" = "Darwin" ]; then
     alias u="brew update && brew upgrade"
 fi
